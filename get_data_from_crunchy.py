2a2c50208787dc48ef18ee908378c4584a86ed50
Kate Marvel <kdm2144@columbia.edu>
2018-08-24 13:58:12 -0400

    fixed bug in X.pr

--- get_data_from_crunchy.py
+++ get_data_from_crunchy.py
@@ -37,7 +37,7 @@ cdms.setNetcdfDeflateLevelFlag(0)
 
 
 def sahel_average(X):
-    if X.id="pr":
+    if X.id=="pr":
         X=X*60*60*24
     sahel = cdutil.region.domain(latitude=(10,20),longitude=(-20,30))
     sahel_average=cdutil.averager(X(sahel),axis='xy')
@@ -45,14 +45,14 @@ def sahel_average(X):
     
 
 def sahel_west(X):
-    if X.id="pr":
+    if X.id=="pr":
         X=X*60*60*24
     sahel_west = cdutil.region.domain(latitude=(10,20),longitude=(-20,0))
     sahel_average_west=cdutil.averager(X(sahel_west),axis='xy')
     return sahel_average_west
 
 def sahel_east(X):
-    if X.id="pr":
+    if X.id=="pr":
         X=X*60*60*24
     sahel_east = cdutil.region.domain(latitude=(10,20),longitude=(0,30))
     sahel_average_east=cdutil.averager(X(sahel_east),axis='xy')
