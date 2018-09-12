decaab21d33374995e0bbd64cf59bfc3db50d720
Kate Marvel <kdm2144@columbia.edu>
2018-09-12 17:31:54 -0400

    fixed time axis bugs

--- get_data_from_crunchy.py
+++ get_data_from_crunchy.py
@@ -36,26 +36,49 @@ cdms.setNetcdfDeflateFlag(0)
 cdms.setNetcdfDeflateLevelFlag(0)
 
 
-def sahel_average(X):
+def sahel_average_hist(X):
     if X.id=="pr":
         X=X*60*60*24
     sahel = cdutil.region.domain(latitude=(10,20),longitude=(-20,30))
-    sahel_average=cdutil.averager(X(sahel),axis='xy')
+    sahel_average=cdutil.averager(X(sahel),axis='xy')(time=('1860-1-1','2006-1-1'))
     return sahel_average
     
 
-def sahel_west(X):
+def sahel_west_hist(X):
     if X.id=="pr":
         X=X*60*60*24
     sahel_west = cdutil.region.domain(latitude=(10,20),longitude=(-20,0))
-    sahel_average_west=cdutil.averager(X(sahel_west),axis='xy')
+    sahel_average_west=cdutil.averager(X(sahel_west),axis='xy')(time=('1860-1-1','2006-1-1'))
     return sahel_average_west
 
-def sahel_east(X):
+def sahel_east_hist(X):
     if X.id=="pr":
         X=X*60*60*24
     sahel_east = cdutil.region.domain(latitude=(10,20),longitude=(0,30))
-    sahel_average_east=cdutil.averager(X(sahel_east),axis='xy')
+    sahel_average_east=cdutil.averager(X(sahel_east),axis='xy')(time=('1860-1-1','2006-1-1'))
+    return sahel_average_east
+
+
+def sahel_average_rcp(X):
+    if X.id=="pr":
+        X=X*60*60*24
+    sahel = cdutil.region.domain(latitude=(10,20),longitude=(-20,30))
+    sahel_average=cdutil.averager(X(sahel),axis='xy')(time=('2006-1-1','2100-1-1'))
+    return sahel_average
+    
+
+def sahel_west_rcp(X):
+    if X.id=="pr":
+        X=X*60*60*24
+    sahel_west = cdutil.region.domain(latitude=(10,20),longitude=(-20,0))
+    sahel_average_west=cdutil.averager(X(sahel_west),axis='xy')(time=('2006-1-1','2100-1-1'))
+    return sahel_average_west
+
+def sahel_east_rcp(X):
+    if X.id=="pr":
+        X=X*60*60*24
+    sahel_east = cdutil.region.domain(latitude=(10,20),longitude=(0,30))
+    sahel_average_east=cdutil.averager(X(sahel_east),axis='xy')(time=('2006-1-1','2100-1-1'))
     return sahel_average_east
 
 def sahel_average_piC(X):
@@ -82,13 +105,17 @@ def sahel_east_piC(X):
 
 def sahel_indicators(experiment):
     if experiment == "piControl":
-        east = cmip5.get_ensemble(experiment,"pr",func=sahel_east)
-        west = cmip5.get_ensemble(experiment,"pr",func=sahel_west)
-        sav = cmip5.get_ensemble(experiment,"pr",func=sahel_average)
-    else:
         east = cmip5.get_ensemble(experiment,"pr",func=sahel_east_piC)
         west = cmip5.get_ensemble(experiment,"pr",func=sahel_west_piC)
         sav = cmip5.get_ensemble(experiment,"pr",func=sahel_average_piC)
+    elif experiment.find("rcp")>=0:
+        east = cmip5.get_ensemble(experiment,"pr",func=sahel_east_rcp)
+        west = cmip5.get_ensemble(experiment,"pr",func=sahel_west_rcp)
+        sav = cmip5.get_ensemble(experiment,"pr",func=sahel_average_rcp)
+    else:
+        east = cmip5.get_ensemble(experiment,"pr",func=sahel_east_hist)
+        west = cmip5.get_ensemble(experiment,"pr",func=sahel_west_hist)
+        sav = cmip5.get_ensemble(experiment,"pr",func=sahel_average_hist)
     
     east.id="pr_CE"
     west.id="pr_W"
